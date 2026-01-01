bf1afa77bdee85169cdc44dc2576832d652e28ef
aftab6363 <kingggzerooo@gmail.com>
2026-01-01 12:08:35 +0500

    Fix: Add MongoDB connection timeout to prevent hanging

--- src/lib/db.js
+++ src/lib/db.js
@@ -20,6 +20,8 @@ async function dbConnect() {
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
+            serverSelectionTimeoutMS: 5000, // Fail after 5 seconds if cannot connect
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
         };
 
         cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
